fd2eb6ac327d0c606da1bdbeb98e1869d124e7b8
costamarcelow <50969725+costamarcelow@users.noreply.github.com>
2021-12-30 15:31:58 -0300

    Modal
    Alterações no Modal, bug no room, caixa wrapper não abre

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,25 @@
-import { Modal } from './modal'
+import Modal from './modal.js'
 
 const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
 //Pegar todos os bot~eos que existe com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
   //adicionar a escuta
-  button.eventlistener('click', event => {
+  button.addEventListener('click', event => {
+    modal.open()
+  })
+})
+
+const deleteButton = document.querySelectorAll('.actions a.delete')
+
+deleteButton.forEach(button => {
+  button.addEventListener('click', event => {
     modal.open()
   })
 })
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,7 +1,14 @@
-module.exports = function Modal() {
+export default function Modal() {
+  const modalWrapper = document.querySelector('.modal-wrapper')
+  const cancelButton = document.querySelector('.button.cancel')
+
+  cancelButton.addEventListener('click', close)
+
   function open() {}
+  modalWrapper.classList.add('active')
 
   function close() {}
+  modalWrapper.classList.remove('active')
 
   return {
     open,
